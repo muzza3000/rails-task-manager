d89a07bf4c52fb63eb31b84f33f820a74939875a
Thomas Murray <thomas.murray09@gmail.com>
2020-09-22 13:55:39 +0200

    CRUD Complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new'
 
   get '/tasks/:id', to: 'tasks#show', as: :task
-
   post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
+# Rails.application.routes.draw do
+#   resources :tasks
+# end
